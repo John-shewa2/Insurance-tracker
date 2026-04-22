5b9df7f0dc33f8418048ba1372bc091c787f49d8
Yohannes <john.shewakena@gmail.com>
2026-04-22 11:51:35 +0300

    edited server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const User = require('./models/User');
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URI || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json());
