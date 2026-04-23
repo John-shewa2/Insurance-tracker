11cf840c2768102a9a9f1727706af5dae7a8f8da
John-shewa2 <john.shewakena@gmail.com>
2026-04-23 15:45:01 +0300

    Change assetCode uniqueness from true to false

--- Backend/models/project.js
+++ Backend/models/project.js
@@ -6,7 +6,7 @@ const projectSchema = new mongoose.Schema({
     listFixedAsset: { type: String, required: true },
     assetCode: { 
         type: String, 
-        unique: true, 
+        unique: false, 
         sparse: true, 
         required: false,
         trim: true 
